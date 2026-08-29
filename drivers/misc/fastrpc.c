29059d3c62cc7e491249cf0d91cef3e4754ad54f
MCC45TR <mursitcancihan@gmail.com>
2026-08-29 13:03:09 +0300

    misc: fastrpc: validate SCM VMID lists

--- drivers/misc/fastrpc.c
+++ drivers/misc/fastrpc.c
@@ -2,6 +2,7 @@
 // Copyright (c) 2011-2018, The Linux Foundation. All rights reserved.
 // Copyright (c) 2018, Linaro Limited
 
+#include <linux/bits.h>
 #include <linux/completion.h>
 #include <linux/device.h>
 #include <linux/dma-buf.h>
@@ -2345,7 +2346,7 @@ static int fastrpc_rpmsg_probe(struct rpmsg_device *rpdev)
 {
 	struct device *rdev = &rpdev->dev;
 	struct fastrpc_channel_ctx *data;
-	int i, err, domain_id = -1, matrix_count, vmcount;
+	int i, j, err, domain_id = -1, matrix_count, vmcount;
 	const char *domain;
 	const char *matrix[9];
 	bool secure_dsp;
@@ -2374,10 +2375,26 @@ static int fastrpc_rpmsg_probe(struct rpmsg_device *rpdev)
 
 	vmcount = of_property_read_variable_u32_array(rdev->of_node,
 				"qcom,vmids", &vmids[0], 0, FASTRPC_MAX_VMIDS);
-	if (vmcount < 0)
+	if (vmcount == -EINVAL)
 		vmcount = 0;
+	else if (vmcount < 0)
+		return dev_err_probe(rdev, vmcount, "invalid qcom,vmids\n");
 	else if (!qcom_scm_is_available())
 		return -EPROBE_DEFER;
+
+	for (i = 0; i < vmcount; i++) {
+		if (vmids[i] >= BITS_PER_TYPE(u64))
+			return dev_err_probe(rdev, -ERANGE,
+					     "qcom,vmids value %u exceeds ownership mask\n",
+					     vmids[i]);
+
+		for (j = 0; j < i; j++) {
+			if (vmids[i] == vmids[j])
+				return dev_err_probe(rdev, -EINVAL,
+						     "duplicate qcom,vmids value %u\n",
+						     vmids[i]);
+		}
+	}
 
 	data = kzalloc(sizeof(*data), GFP_KERNEL);
 	if (!data)
@@ -2431,7 +2448,7 @@ static int fastrpc_rpmsg_probe(struct rpmsg_device *rpdev)
 			goto err_free_data;
 		}
 
-		src_perms = BIT(QCOM_SCM_VMID_HLOS);
+		src_perms = BIT_ULL(QCOM_SCM_VMID_HLOS);
 		err = qcom_scm_assign_mem(res.start, resource_size(&res),
 					  &src_perms, data->vmperms,
 					  data->vmcount);
